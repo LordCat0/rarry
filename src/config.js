d9e339a453714437045bbd736110d328563380b5
ddededodediamante <lukitadiamante5@gmail.com>
2025-11-01 20:25:26 -0300

    Why

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const localhost = window.location.hostname === "localhost";
 
 export default {
-  apiUrl: localhost ? "http://rarry-api.onthewifi.com:14524" : "",
+  apiUrl: localhost ? "http://localhost:3000" : "http://rarry-api.onthewifi.com:14524",
 };
